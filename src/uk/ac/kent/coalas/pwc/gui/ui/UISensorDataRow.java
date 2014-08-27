76c60a50118d88fe44c0d2965c56066b84f6c920
Richard Miles <shaggythesheep>
2014-08-27 12:58:34 +0100

    Enabled timeout feedback

--- src/uk/ac/kent/coalas/pwc/gui/ui/UISensorDataRow.java
+++ src/uk/ac/kent/coalas/pwc/gui/ui/UISensorDataRow.java
@@ -10,6 +10,9 @@ import java.util.Random;
 
 /**
  * Created by rm538 on 19/08/2014.
+ *
+ * A class for handling drawing sensor data to the screen
+ *
  */
 public class UISensorDataRow extends UIObject implements RowPositionTracker.YIncrementer{
 
@@ -17,14 +20,14 @@ public class UISensorDataRow extends UIObject implements RowPositionTracker.YInc
     private Sensor sensor;
     private RowPositionTracker positionTracker;
 
-    private static int BAR_HEIGHT = 10;
-    private static int BAR_MAX_WIDTH = 200;
+    private int BAR_HEIGHT = 10;
+    private int BAR_MAX_WIDTH = 225;
 
-    private static int BAR_FILL_COLOUR_US = 0xFF00FF00;
-    private static int BAR_FILL_COLOUR_IR = 0xFFFF0000;
-    private static int BAR_FILL_COLOUR_FUSED = 0xFF0000FF;
+    private int BAR_FILL_COLOUR_US = 0xFF68DC52;
+    private int BAR_FILL_COLOUR_IR = 0xFFFF463B;
+    private int BAR_FILL_COLOUR_FUSED = 0xFF0F89D4;
 
-    private static int TEXT_SIZE = 10;
+    private int TEXT_SIZE = 10;
 
     public UISensorDataRow(WheelchairGUIFrame parent, Sensor sensor, RowPositionTracker positionTracker){
 
@@ -35,43 +38,46 @@ public class UISensorDataRow extends UIObject implements RowPositionTracker.YInc
 
     public void draw(){
 
+        // If the sensor that this row relates to doesn't exist, quit
         if(sensor == null){
             return;
         }
 
+        // Align ourselves to the left
         positionTracker.resetX();
 
+        // Output the current value
         parent.fill(50);
         parent.textSize(TEXT_SIZE);
         parent.textAlign(PConstants.CENTER);
         parent.text(sensor.getCurrentValue(), positionTracker.getX(), positionTracker.getY() + TEXT_SIZE);
         positionTracker.incrementXPosition(25);
 
+        // Output the interpretation of this value
         parent.textAlign(PConstants.CENTER);
         parent.text(sensor.getDataInterpretation().getSuffix(), positionTracker.getX(), positionTracker.getY() + TEXT_SIZE);
         positionTracker.incrementXPosition(15);
 
-        double barWidth;
-        int currentValue;
 
+        // Scale the bar to represent the value
+        double barWidth, barWidthProportion;
+
+        // If the sensor's data is boolean, either make the bar visible or not
         if(sensor.getDataInterpretation() == Sensor.SensorDataInterpretation.THRESHOLD){
-            barWidth = BAR_MAX_WIDTH;
-            currentValue = (sensor.getCurrentBoolValue() ? 1 : 0);
+            barWidthProportion = (sensor.getCurrentBoolValue() ? 1 : 0);
         } else {
-            barWidth = ((double) BAR_MAX_WIDTH / (double) DiagnosticsFrame.SCALE_MAX);
-            currentValue = sensor.getCurrentValue();
+            // Otherwise, calculate the
+            barWidthProportion = (double) sensor.getCurrentValue() / (double) DiagnosticsFrame.SCALE_MAX;
 
-            System.out.println(parent.millis());
-            System.out.println(parent.millis() - DiagnosticsFrame.lastUpdateTime);
-            System.out.println(DiagnosticsFrame.updatePeriod);
-
-            if(parent.frameCount % (DiagnosticsFrame.updatePeriod / 100) == 0) {
+            // TODO: Remove before production
+            if(parent.frameCount % 30 == 0) {
                 sensor.parseDataString(String.format("%04x", new Random().nextInt(DiagnosticsFrame.SCALE_MAX)));
             }
         }
 
-        barWidth = barWidth * currentValue;
+        barWidth = BAR_MAX_WIDTH * barWidthProportion;
 
+        // Set the colour of the bar, depending on the sensor type
         if(sensor.getType() == Sensor.SensorType.ULTRASONIC) {
             parent.fill(BAR_FILL_COLOUR_US);
         } else if(sensor.getType() == Sensor.SensorType.INFRARED){
@@ -79,13 +85,16 @@ public class UISensorDataRow extends UIObject implements RowPositionTracker.YInc
         } else if(sensor.getType() == Sensor.SensorType.FUSED){
             parent.fill(BAR_FILL_COLOUR_FUSED);
         }
+        // Draw the bar
         parent.stroke(0);
         parent.rect(positionTracker.getX(), positionTracker.getY(), (int) barWidth, BAR_HEIGHT);
 
+        // Output the type of sensor as an additional reminder
         parent.fill(50);
         parent.textAlign(PConstants.CENTER);
         parent.text(sensor.getType().getName(), 300, positionTracker.getY() + TEXT_SIZE);
 
+        // Move down the screen, ready for the next row
         positionTracker.incrementYPosition(this);
     }
 
