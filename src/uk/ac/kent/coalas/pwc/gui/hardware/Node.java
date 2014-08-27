cdebd90749c40596816500013cc14149ad8d8124
Richard Miles <shaggythesheep>
2014-08-27 12:56:44 +0100

    Enabled timeout access to timeout status

--- src/uk/ac/kent/coalas/pwc/gui/hardware/Node.java
+++ src/uk/ac/kent/coalas/pwc/gui/hardware/Node.java
@@ -11,22 +11,26 @@ public class Node {
 
     public static final int MAX_ZONES = 3;
 
-    private PWCInterface chairInterface;
     private int id;
     private ArrayList<Zone> zones = new ArrayList<Zone>(MAX_ZONES);
     private ArrayList<Integer> sensorDataOrder = new ArrayList<Integer>();
     private boolean connected = false;
     private boolean dataFormatKnown = true; // TODO: Change this to false for production
+    private boolean hasTimedOut = false;
 
-    public Node(PWCInterface charInterface, int nodeId){
+    public Node(int nodeId){
 
-        this.chairInterface = charInterface;
         id = nodeId;
 
         // Each node has a maximum of 3 zones - lets create some empty ones ready for use later
         for(int i = 0; i < MAX_ZONES; i++){
             zones.add(i, new Zone(this, i+1, Zone.Position.UNKNOWN, Zone.Orientation.UNKNOWN));
         }
+    }
+
+    public boolean hasTimedOut(){
+
+        return hasTimedOut;
     }
 
     public boolean isConnectedToBus(){
@@ -37,6 +41,11 @@ public class Node {
     public void setConnectedToBus(boolean connectedToBus){
 
         connected = connectedToBus;
+    }
+
+    public void setTimedOut(boolean timedOut){
+
+        hasTimedOut = timedOut;
     }
 
     public int getId(){
@@ -72,7 +81,11 @@ public class Node {
     public Zone getZone(int zoneNumber){
 
         int zoneNumberMasked = zoneNumber  & 0x3; // Bitmask = Ox3 -> 11 (as we only want to get the first 2 bits)
-        return zones.get(zoneNumberMasked - 1);
+        if(zoneNumberMasked > 0) {
+            return zones.get(zoneNumberMasked - 1);
+        } else {
+            return null;
+        }
     }
 
     public boolean isDataFormatKnown(){
@@ -88,50 +101,5 @@ public class Node {
     public void setZone(int zoneNumber, Zone newZone){
 
         zones.set(zoneNumber - 1, newZone);
-    }
-
-
-
-
-
-
-    public void checkExistsOnBus(){
-
-        chairInterface.sendCommand("&" + String.valueOf(getId()) + "S");
-    }
-
-    public void configureSensors(String configString){
-
-        chairInterface.sendCommand("&" + String.valueOf(getId()) + "C" + configString);
-    }
-
-    public void requestConfiguration(){
-
-        chairInterface.sendCommand("&" + String.valueOf(getId()) + "R");
-    }
-
-    public void requestCurrentData(){
-
-        chairInterface.sendCommand("&" + String.valueOf(getId()) + "D");
-    }
-
-    public void requestDataFormat(){
-
-        chairInterface.sendCommand("&" + String.valueOf(getId()) + "F");
-    }
-
-    public void setThresholds(int zone1Threshold, int zone2Threshold, int zone3Threshold){
-
-        chairInterface.sendCommand("&" + String.valueOf(getId()) + "T" +
-                chairInterface.intTo12BitHex(zone1Threshold) +
-                chairInterface.intTo12BitHex(zone2Threshold) +
-                chairInterface.intTo12BitHex(zone3Threshold));
-    }
-
-    public void setUltrasoundMode(PWCInterface.UltrasoundMode mode){
-
-        char modeStr = mode.getCharCode();
-
-        chairInterface.sendCommand("&" + String.valueOf(getId()) + "M" + modeStr);
     }
 }
