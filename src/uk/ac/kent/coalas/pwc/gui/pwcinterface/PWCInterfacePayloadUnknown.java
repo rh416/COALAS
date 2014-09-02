7b1a460583af3aa98b65c70ecfdf16b4c94d8a5a
Richard Miles <shaggythesheep>
2014-09-02 14:59:04 +0100

    Implemented class to represent an the payload of an Unknown event

---
+++ src/uk/ac/kent/coalas/pwc/gui/pwcinterface/PWCInterfacePayloadUnknown.java
@@ -0,0 +1,10 @@
+package uk.ac.kent.coalas.pwc.gui.pwcinterface;
+
+/**
+ * Created by rm538 on 02/09/2014.
+ */
+public class PWCInterfacePayloadUnknown extends PWCInterfaceEventPayload {
+    public PWCInterfacePayloadUnknown(PWCInterface chairInterface, String response) {
+        super(chairInterface, response);
+    }
+}
