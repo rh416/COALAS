f442b6f1a4d7cc971ea307e1f1c05c0cb17e58b3
Richard Miles <richard.miles@fast-chat.co.uk>
2015-03-27 11:37:37 +0000

    Preparing old project for move

--- src/uk/ac/kent/coalas/pwc/gui/pwcinterface/PWCInterfaceListener.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.kent.coalas.pwc.gui.pwcinterface;
-
-/**
- * Created by rm538 on 06/08/2014.
- */
-public interface PWCInterfaceListener {
-
-    public void onPWCInterfaceEvent(PWCInterfaceEvent e);
-}
